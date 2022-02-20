b7913ef0476379a41c7bc2678013f5155adc89db
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2022-02-20 18:48:17 +0300

    0.0.1

--- Api/Components/Display/Display.js
+++ Api/Components/Display/Display.js
@@ -1,7 +1,15 @@
 import {Component} from '../Component.js';
+import {Measure} from '../Measure.js'
 
 
-class Display extends Component {}
+class Display extends Component {
+  async _build() {
+    await super._build();
+    
+    Measure.init(this.body, 'length')
+  }
+  
+}
 
 
 Display.init(import.meta.url);
--- Api/Components/Measure.js
+++ Api/Components/Measure.js
@@ -7,31 +7,66 @@ export class Measure {
     'meter',
     'kilometer',
     'inch',
+    'foot',
+    'yard',
+    'mile',
+    'nautical mile',
   ]
   _volume = [
-    'milliliter',
+    'cubic centimeter',
+    'cubic detsimeter',
+    'cubic inch',
+    'cubic inch',
+    'oil barrel',
+    'cubic metr',
+    'cubic millimeter',
+    'cubic yard',
     'liter',
-    'kubic millimeter',
-    'kubic centimeter',
-    'kubic detsimeter',
-    'kubic metr',
+    'milliliter',
   ]
   _mass = [
-    'gram',
-    'kilogram',
     'centner',
-    'tons',
+    'gram',
+    'gran',
+    'gross ton',
+    'kilogram',
+    'ounce',
     'pound',
+    'short ton',
     'stone',
+    'tons',
   ]
   _area = [
-    
+    'acre',
+    'are',
+    'hectare',
+    'square centimeter',
+    'square decimeter',
+    'square foot',
+    'square inch',
+    'square kilometre',
+    'square millimeter',
+    'square yard',
   ]
-
-
-
-
-  init(body, type) {
+  
+  
+  static _distibutor(body, type) {
+    let lists = body.querySelectorAll('select');
     
+    for (let i = 0; i < 2; i++) {
+      for (let k = 0; k < 10; k++) {
+        let option = document.createElement('option');
+        
+        option.textContent = this._[type][k];
+        
+        lists[i].append(option);
+      }
+    }
+  }
+
+
+
+  static init(body, type) {
+    this._distibutor(body, type);
   }
 }
