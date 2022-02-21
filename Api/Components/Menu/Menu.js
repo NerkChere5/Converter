00a77a90ef969e3a0164dce99af3a867ac4709c6
NerkChere5 <87468612+NerkChere5@users.noreply.github.com>
2022-02-21 11:09:15 +0300

    0.1.2

--- Api/Components/Menu/Menu.js
+++ Api/Components/Menu/Menu.js
@@ -3,37 +3,7 @@ import {Component} from '../Component.js';
 
 
 
-class Menu extends Component {
-  _flod_menu = null;
-  _list_menu = null;
-  
-  async _build() {
-    await super._build();
-    
-    this._flod_menu = this._body.querySelector('.flod_menu');
-    this._list_menu = this._body.querySelector('.list_menu');
-    
-    this._body.addEventListener('click', this._on_click.bind(this));
-  }
-  
-  _on_click(event) {
-    if (event.target.classList.contains('flod')) this.close_menu();
-    if (event.target.classList.contains('show_menu')) this.show_menu();
-  }
-  
-  
-  
-  
-  close_menu() {
-    this._list_menu.setAttribute('hidden', 'true');
-    this._flod_menu.removeAttribute('hidden');
-  }
-  
-  show_menu() {
-    this._flod_menu.setAttribute('hidden', 'true');
-    this._list_menu.removeAttribute('hidden');
-  }
-}
+class Menu extends Component {}
 
 
 Menu.init(import.meta.url);
